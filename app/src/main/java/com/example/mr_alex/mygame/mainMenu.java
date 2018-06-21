01a398e01bb10b4ff0e92911de15349fd3bef826
Alexander Manakov <alexander.pozitiv@yandex.ru>
2018-06-21 23:56:46 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mr_alex/mygame/mainMenu.java
@@ -0,0 +1,13 @@
+package com.example.mr_alex.mygame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+    }
+}
---
+++ app/src/main/java/com/example/mr_alex/mygame/playerVSplayer.java
@@ -0,0 +1,13 @@
+package com.example.mr_alex.mygame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class playerVSplayer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.player_vs_player);
+    }
+}
