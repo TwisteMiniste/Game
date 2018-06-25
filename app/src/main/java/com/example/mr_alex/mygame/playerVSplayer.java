6e1a42ac6a9adec12e6e942d2d4ca8322a2464d9
Alexander Manakov <alexander.pozitiv@yandex.ru>
2018-06-25 21:45:59 +0300

    playerVSplayer

--- app/src/main/java/com/example/mr_alex/mygame/playerVSplayer.java
+++ app/src/main/java/com/example/mr_alex/mygame/playerVSplayer.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,31 +14,11 @@ public class playerVSplayer extends AppCompatActivity {
     TextView tvPlayer_1, tvPlayer_2;
     TextView etPlayer_1, etPlayer_2;
     Button
-    num_1,
-    num_2,
-    num_3,
-    num_4,
-    num_5,
-    num_6,
-    num_7,
-    num_8,
-    num_9,
-    num_0,
-    button_delete,
-    button_enter;
+            button_delete,
+            button_enter;
     Button
-    num_1_,
-    num_2_,
-    num_3_,
-    num_4_,
-    num_5_,
-    num_6_,
-    num_7_,
-    num_8_,
-    num_9_,
-    num_0_,
-    button_delete_,
-    button_enter_;
+            button_delete_,
+            button_enter_;
 
     int diff = 100;
     int answer;
@@ -55,28 +33,21 @@ public class playerVSplayer extends AppCompatActivity {
         tvPlayer_2 = findViewById(R.id.tvPlayer_2);
         etPlayer_1 = findViewById(R.id.etPlayer_1);
         etPlayer_2 = findViewById(R.id.etPlayer_2);
-        num_1 = findViewById(R.id.num_1);
-        num_2 = findViewById(R.id.num_2);
-        num_3 = findViewById(R.id.num_3);
-        num_4 = findViewById(R.id.num_4);
-        num_5 = findViewById(R.id.num_5);
-        num_6 = findViewById(R.id.num_6);
-        num_7 = findViewById(R.id.num_7);
-        num_8 = findViewById(R.id.num_8);
-        num_9 = findViewById(R.id.num_9);
-        num_0 = findViewById(R.id.num_0);
+
         button_delete = findViewById(R.id.button_delete);
         button_enter = findViewById(R.id.button_enter);
+        button_delete_ = findViewById(R.id.button_delete_);
+        button_enter_ = findViewById(R.id.button_enter_);
 
         answer = (int)(Math.random()*diff);
 
-        tvPlayer_1.setText(getResources().getString(R.string.app_name));
-        tvPlayer_2.setText(getResources().getString(R.string.app_name));
+        tvPlayer_1.setText(getString(R.string.app_name));
+        tvPlayer_2.setText(getString(R.string.app_name));
 
         gameFinished = false;
     }
 
-    public void onClick1(View v) {
+    public void onClick (View v) {
         if (!gameFinished) {
             switch (v.getId()) {
                 case R.id.num_1: {
@@ -121,34 +92,6 @@ public class playerVSplayer extends AppCompatActivity {
                     }
                 }
                 break;
-            }
-        }
-        switch (v.getId()) {
-            case R.id.button_delete: {
-                if (!gameFinished) {
-                    etPlayer_1.setText(R.string.empty);
-                } else {
-                    playagain_no();
-                }
-            }
-            break;
-            case R.id.button_enter: {
-                if (!gameFinished) {
-                    compare1(etPlayer_1.getText().toString());
-                    etPlayer_1.setText(R.string.empty);
-                } else {
-                    playagain_yes1();
-                }
-            }
-            break;
-            default:
-                break;
-        }
-    }
-
-    public void onClick2(View v) {
-        if (!gameFinished) {
-            switch (v.getId()) {
                 case R.id.num_1_: {
                     setTvInput2('1');
                 }
@@ -194,6 +137,23 @@ public class playerVSplayer extends AppCompatActivity {
             }
         }
         switch (v.getId()) {
+            case R.id.button_delete: {
+                if (!gameFinished) {
+                    etPlayer_1.setText(R.string.empty);
+                } else {
+                    playagain_no();
+                }
+            }
+            break;
+            case R.id.button_enter: {
+                if (!gameFinished) {
+                    compare1(etPlayer_1.getText().toString());
+                    etPlayer_1.setText(R.string.empty);
+                } else {
+                    playagain_yes();
+                }
+            }
+            break;
             case R.id.button_delete_: {
                 if (!gameFinished) {
                     etPlayer_2.setText(R.string.empty);
@@ -207,40 +167,39 @@ public class playerVSplayer extends AppCompatActivity {
                     compare2(etPlayer_2.getText().toString());
                     etPlayer_2.setText(R.string.empty);
                 } else {
-                    playagain_yes2();
+                    playagain_yes();
                 }
             }
             break;
-            default:
-                break;
+            default: break;
         }
     }
 
     private void setTvInput1 (char ch){
-        String strInput1;
+        String strInput;
         if (!etPlayer_1.getText().toString().equals("0")){
-            strInput1 = etPlayer_1.getText().toString() + ch;
+            strInput = etPlayer_1.getText().toString() + ch;
         } else {
-            strInput1 = "" + ch;
+            strInput = "" + ch;
         }
-        if (strInput1.length() <= 6) {
-            etPlayer_1.setText(strInput1);
+        if (strInput.length() <= 6) {
+            etPlayer_1.setText(strInput);
         } else {
-            Toast.makeText(this,"Слишком большое число!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this,getString(R.string.overflow), Toast.LENGTH_SHORT).show();
         }
     }
 
     private void setTvInput2 (char ch){
-        String strInput2;
+        String strInput;
         if (!etPlayer_2.getText().toString().equals("0")){
-            strInput2 = etPlayer_2.getText().toString() + ch;
+            strInput = etPlayer_2.getText().toString() + ch;
         } else {
-            strInput2 = "" + ch;
+            strInput = "" + ch;
         }
-        if (strInput2.length() <= 6) {
-            etPlayer_2.setText(strInput2);
+        if (strInput.length() <= 6) {
+            etPlayer_2.setText(strInput);
         } else {
-            Toast toast = Toast.makeText(getApplicationContext(), "Слишком большое число!", Toast.LENGTH_LONG);
+            Toast toast = Toast.makeText(getApplicationContext(), getString(R.string.overflow), Toast.LENGTH_LONG);
             View toastView = toast.getView();
             toastView.setRotation(180);
             toast.setGravity(Gravity.TOP, 0, 0);
@@ -248,37 +207,25 @@ public class playerVSplayer extends AppCompatActivity {
         }
     }
 
-    private void playagain_yes1 () {
+    private void playagain_yes () {
         answer = (int) (Math.random() * diff);
 
-        tvPlayer_1.setText(getResources().getString(R.string.app_name));
-        button_delete.setText(getResources().getString(R.string.num_delete));
-        button_enter.setText(getResources().getString(R.string.num_enter));
-        /*tvPlayer_2.setText(getResources().getString(R.string.app_name));
-        button_delete_.setText(getResources().getString(R.string.num_delete));
-        button_enter_.setText(getResources().getString(R.string.num_enter));
-*/
-        gameFinished = false;
-    }
-
-    private void playagain_yes2 () {
-        answer = (int) (Math.random() * diff);
-
-     /*   tvPlayer_1.setText(getResources().getString(R.string.app_name));
-        button_delete.setText(getResources().getString(R.string.num_delete));
-        button_enter.setText(getResources().getString(R.string.num_enter));*/
-        tvPlayer_2.setText(getResources().getString(R.string.app_name));
-        button_delete_.setText(getResources().getString(R.string.num_delete));
-        button_enter_.setText(getResources().getString(R.string.num_enter));
+        tvPlayer_1.setText(getString(R.string.app_name));
+        button_delete.setText(getString(R.string.num_delete));
+        button_enter.setText(getString(R.string.num_enter));
+        tvPlayer_2.setText(getString(R.string.app_name));
+        button_delete_.setText(getString(R.string.num_delete));
+        button_enter_.setText(getString(R.string.num_enter));
 
         gameFinished = false;
     }
 
-    private void playagain_no () {
-        Intent mainMenu = new Intent(this, mainMenu.class);
-        startActivity(mainMenu);
-        finish();
-    }
+        private void playagain_no () {
+            Intent mainMenu = new Intent(this, mainMenu.class);
+            startActivity(mainMenu);
+            finish();
+        }
+
 
     private void compare1 (String str) {
         if (!etPlayer_1.getText().toString().equals("")) {
@@ -286,27 +233,22 @@ public class playerVSplayer extends AppCompatActivity {
                 int inp = Integer.parseInt(str);
 
                 if (inp > answer) {
-                    tvPlayer_1.setText(getResources().getString(R.string.less_than_input));
+                    tvPlayer_1.setText(getString(R.string.less_than_input));
                 }
 
                 if (inp < answer) {
-                    tvPlayer_1.setText(getResources().getString(R.string.greater_than_input));
+                    tvPlayer_1.setText(getString(R.string.greater_than_input));
                 }
 
                 if (inp == answer) {
-                    tvPlayer_1.setText(getResources().getString(R.string.player1_win));
-          //          tvPlayer_2.setText(getResources().getString(R.string.player2_lose));
+                    tvPlayer_1.setText(getString(R.string.player1_win));
+                    tvPlayer_2.setText(getString(R.string.player2_lose));
 
-                    button_delete.setText(getResources().getString(R.string.no));
-                    button_enter.setText(getResources().getString(R.string.yes));
-                //    button_delete_.setText(getResources().getString(R.string.no));
-                  //  button_enter_.setText(getResources().getString(R.string.yes));
-
-                    gameFinished = true;
+                    setGameFinished();
                 }
             }
         } else {
-            Toast.makeText(this, "Пустое поле!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, getString(R.string.empty_field), Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -316,29 +258,33 @@ public class playerVSplayer extends AppCompatActivity {
                 int inp = Integer.parseInt(str);
 
                 if (inp > answer) {
-                    tvPlayer_2.setText(getResources().getString(R.string.less_than_input));
+                    tvPlayer_2.setText(getString(R.string.less_than_input));
                 }
 
                 if (inp < answer) {
-                    tvPlayer_2.setText(getResources().getString(R.string.greater_than_input));
+                    tvPlayer_2.setText(getString(R.string.greater_than_input));
                 }
 
                 if (inp == answer) {
-                    tvPlayer_2.setText(getResources().getString(R.string.player2_win));
-             //       tvPlayer_1.setText(getResources().getString(R.string.player1_lose));
+                    tvPlayer_2.setText(getString(R.string.player2_win));
+                    tvPlayer_1.setText(getString(R.string.player1_lose));
 
-                    button_delete_.setText(getResources().getString(R.string.no));
-                    button_enter_.setText(getResources().getString(R.string.yes));
-                //    button_delete.setText(getResources().getString(R.string.no));
-                  //  button_enter.setText(getResources().getString(R.string.yes));
-
-                    gameFinished = true;
+                    setGameFinished();
                 }
             }
-        } else {Toast toast = Toast.makeText(getApplicationContext(), "Пустое поле!", Toast.LENGTH_LONG);
+        } else {Toast toast = Toast.makeText(getApplicationContext(), getString(R.string.empty_field), Toast.LENGTH_LONG);
             View toastView = toast.getView();
             toastView.setRotation(180);
             toast.setGravity(Gravity.TOP, 0, 0);
             toast.show();}
+    }
+
+    private void setGameFinished (){
+        button_delete_.setText(getString(R.string.no));
+        button_enter_.setText(getString(R.string.yes));
+        button_delete.setText(getString(R.string.no));
+        button_enter.setText(getString(R.string.yes));
+
+        gameFinished = true;
     }
 }
